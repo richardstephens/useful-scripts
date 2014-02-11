1549209d6a1dc58a2f784bf1e1db2f2aaa518e78
Richard Stephens <rstephens@atlassian.com>
2014-02-11 16:36:18 +1100

    incldue time difference between this and last line

--- consoletimestamp.py
+++ consoletimestamp.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python -u
 import datetime, sys
 
+lasttime = datetime.datetime.now()
 while True:
   line = sys.stdin.readline()
   if not line: break
   now = datetime.datetime.now()
   st = now.strftime('%H:%M:%S.%f')[:-3]
-  print st, " ", line.rstrip('\n')
+  diff = now - lasttime
+  print st, " ", str(diff), " ", line.rstrip('\n')
+  lasttime = now
 
