f2865dd6f4e85ca3d786d43a4b532b879d6927af
Richard Stephens <rstephens@atlassian.com>
2014-02-13 11:18:41 +1100

    add time since start to console timestamp script

--- consoletimestamp.py
+++ consoletimestamp.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python -u
+# script to take input from stdin and print a timestamp next to it
+# prints time of entry, time since start, time since last entry
+
 import datetime, sys
 
+starttime = datetime.datetime.now()
 lasttime = datetime.datetime.now()
 while True:
   line = sys.stdin.readline()
@@ -8,6 +12,7 @@ while True:
   now = datetime.datetime.now()
   st = now.strftime('%H:%M:%S.%f')[:-3]
   diff = now - lasttime
-  print st, " ", str(diff), " ", line.rstrip('\n')
+  diffstart = now - starttime
+  print st, " ", str(diffstart)[:-3], " ", str(diff)[:-3], " ", line.rstrip('\n')
   lasttime = now
 
